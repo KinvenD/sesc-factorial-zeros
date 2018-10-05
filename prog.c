59f1e862cc40f593910bf6d532304dd2e280d25a
KinvenD <37690427+KinvenD@users.noreply.github.com>
2018-10-05 21:03:06 +0700

    just some text that wont give an error
    just some text that wont give an error

--- prog.c
+++ prog.c
@@ -1,1 +1,15 @@
-wwwwwwww
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int z = 0; 
+  int a = 5;
+  int count = 0;
+  scanf("%d", &z);
+  while((z/a) >= 1){
+    count += (z/a);
+    a*=5;
+  }
+  printf("%d", count);
+  return 0;
+}
